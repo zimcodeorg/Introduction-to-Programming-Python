5005169b29de4f7b10fe65428fe70ec1c67d09fb
Mgcini Keith Phuthi <mkphuthi@gmail.com>
2020-02-23 20:11:38 -0500

    Course Materials
    Uploaded all the latest course materials for conductiing the 12 week program. There will likely be major changes in content and file formats going forward.

---
+++ 12-week Course/Exercises/Longer Exercises Solutions/square_spiral.py
@@ -0,0 +1,7 @@
+import turtle
+
+x = 20
+for i in range(10):
+    turtle.fd(x)
+    turtle.left(90)
+    x += 10
---
+++ 12-week Course/Slides/Lesson 20 Answers.py
@@ -0,0 +1,56 @@
+from Processing import *
+
+#French
+window(1020,720)
+background(255,255,255)
+fill(0,0,255)                  
+rect(0,0,340,720)
+fill(255,0,0)
+rect(680,0,340,720)
+
+#Japanese
+window(1020,720)
+background(255,255,255)     
+fill(255,0,0)
+ellipse(510,360,360,360)
+
+#Zimbabwe
+window(1020,700)
+background(255,255,255)
+fill(0,255,0)
+rect(0,0,1020,100)
+fill(255,255,0)
+rect(0,100,1020,100)
+fill(255,0,0)
+rect(0,200,1020,100)
+fill(0,0,0)
+rect(0,300,1020,100)
+fill(255,0,0)
+rect(0,400,1020,100)
+fill(255,255,0)
+rect(0,500,1020,100)
+fill(0,255,0)
+rect(0,600,1020,100)
+fill(255,255,255)
+triangle(0,0,300,350,0,700)
+
+#Sweden
+window(1020,720)
+background(0,0,240)         
+fill(255,255,0)             
+noStroke()
+rect(310,0,100,720)
+rect(0,310,1020,100)
+
+#Brazil
+window(1020,720)
+background(0,168,89)         #Green
+fill(255,204,41)             #Yellow
+noStroke()
+quad(100,360,510,100,920,360,510,620)
+fill(62,64,149)              #Blue
+ellipse(510,360,330,330)
+
+
+
+               
